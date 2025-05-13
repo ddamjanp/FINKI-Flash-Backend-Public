fc48821afba80cbdf991e808bfa88895154db86b
Sergej <noracsergej@gmail.com>
2025-05-13 23:28:29 +0200

    set remote db

--- backend/src/main/java/com/flash/finki/repository/FileRepository.java
+++ backend/src/main/java/com/flash/finki/repository/FileRepository.java
@@ -2,6 +2,8 @@ package com.flash.finki.repository;
 
 import com.flash.finki.model.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface FileRepository extends JpaRepository<File, Long> {
